32b64465eb24122adcf4b430393df98284b50844
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-27 06:52:30 +0530

    Added Navbar and images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import "@/styles/globals.css";
+import Layout from "@/components/layouts/layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { theme } from "../chakra/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
